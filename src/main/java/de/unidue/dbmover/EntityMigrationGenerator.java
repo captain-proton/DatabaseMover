d453045ec0e6597932b67d74300f1ff2e371a2c7
Nils Verheyen <nverheyen@gmail.com>
2019-06-06 15:31:51 +0200

    Add configurable maximum numbers of loaded records

--- src/main/java/de/unidue/dbmover/EntityMigrationGenerator.java
+++ src/main/java/de/unidue/dbmover/EntityMigrationGenerator.java
@@ -65,9 +65,16 @@ public class EntityMigrationGenerator {
         ClassName className = ClassName.get(migrationBasePackageName, String.join("", "_", objectEntity.getName(), "Mover"));
         TypeSpec typeSpec = TypeSpec.classBuilder(className)
                 .addModifiers(Modifier.PUBLIC)
+                .addField(FieldSpec.builder(Integer.class, "maxLoadedItems")
+                        .addModifiers(Modifier.PRIVATE)
+                        .initializer("200")
+                        .build())
+                .addMethod(buildDefaultConstructor())
+                .addMethod(buildBaseMaxLoadedItemsConstructor())
                 .addMethod(buildMoveMethod(objectEntity))
                 .addMethod(buildMigrateMethod(objectEntity))
                 .addMethod(buildLoadMethod(objectEntity))
+                .addMethod(buildMaxLoadedItemsGetter())
                 .build();
 
         JavaFile javaFile = JavaFile.builder(migrationBasePackageName, typeSpec).build();
@@ -77,6 +84,28 @@ public class EntityMigrationGenerator {
             LOG.error("Could not generate migration class for entity " + objectEntity.getClassName(), e);
         }
         return className;
+    }
+
+    private MethodSpec buildMaxLoadedItemsGetter() {
+        return MethodSpec.methodBuilder("getMaxLoadedItems")
+                .addModifiers(Modifier.PUBLIC)
+                .returns(Integer.class)
+                .addStatement("return $N", "maxLoadedItems")
+                .build();
+    }
+
+    private MethodSpec buildDefaultConstructor() {
+        return MethodSpec.constructorBuilder()
+                .addModifiers(Modifier.PUBLIC)
+                .build();
+    }
+
+    private MethodSpec buildBaseMaxLoadedItemsConstructor() {
+        return MethodSpec.constructorBuilder()
+                .addModifiers(Modifier.PUBLIC)
+                .addParameter(Integer.class, "maxLoadedItems")
+                .addStatement("this.$N = $N", "maxLoadedItems", "maxLoadedItems")
+                .build();
     }
 
     private MethodSpec buildMoveMethod(ObjEntity objEntity) {
@@ -118,8 +147,7 @@ public class EntityMigrationGenerator {
 
         return buildMigrateMethodSignature()
                 .addStatement("int offset = 0")
-                .addStatement("int limit = 200")
-                .addStatement("$T<$T> sourceObjects = load$L(offset, limit, $N)", listClassName, entityClass, entityName, "sourceContext")
+                .addStatement("$T<$T> sourceObjects = load$L(offset, $N)", listClassName, entityClass, entityName, "sourceContext")
                 .beginControlFlow("while ($N.size() > 0)", "sourceObjects")
                 .beginControlFlow("for ($L obj : $N)", entityClassName, "sourceObjects")
                 .addStatement("$T dstObject = $N.newObject($T.class)", entityClass, "destinationContext", entityClass)
@@ -130,8 +158,8 @@ public class EntityMigrationGenerator {
                 .endControlFlow()
                 .endControlFlow()
                 .addStatement("destinationContext.commitChanges()")
-                .addStatement("offset += limit")
-                .addStatement("sourceObjects = load$L(offset, limit, sourceContext)", entityName)
+                .addStatement("offset += $N", "maxLoadedItems")
+                .addStatement("sourceObjects = load$L(offset, sourceContext)", entityName)
                 .endControlFlow()
                 .build();
     }
@@ -141,8 +169,8 @@ public class EntityMigrationGenerator {
         return buildLoadMethodSignature()
                 .addStatement("return $T.query($L.class)" +
                         ".offset(offset)" +
-                        ".limit(limit)" +
-                        ".select(context)", ObjectSelect.class, objEntity.getClassName())
+                        ".limit($N)" +
+                        ".select(context)", ObjectSelect.class, objEntity.getClassName(), "maxLoadedItems")
                 .build();
     }
 
@@ -157,9 +185,12 @@ public class EntityMigrationGenerator {
         if (moverClassFile.exists()) {
             return;
         }
+
         TypeSpec typeSpec = TypeSpec.classBuilder(objectEntity.getName() + "Mover")
                 .superclass(baseClass)
                 .addModifiers(Modifier.PUBLIC)
+                .addMethod(buildDefaultConstructor())
+                .addMethod(buildMaxLoadedItemsConstructor())
                 .addMethod(buildMigrateMethodOverride())
                 .addMethod(buildLoadMethodOverride())
                 .build();
@@ -172,11 +203,20 @@ public class EntityMigrationGenerator {
         }
     }
 
+    private MethodSpec buildMaxLoadedItemsConstructor() {
+
+        return MethodSpec.constructorBuilder()
+                .addModifiers(Modifier.PUBLIC)
+                .addParameter(Integer.class, "maxLoadedItems")
+                .addStatement("super($N)", "maxLoadedItems")
+                .build();
+    }
+
     private MethodSpec buildLoadMethodOverride() {
         return buildLoadMethodSignature()
                 .addAnnotation(Override.class)
                 .addComment("Use this method to implement your own 'load" + objectEntity.getName() + "' implementation")
-                .addStatement("return super.load$L($N, $N, $N)", objectEntity.getName(), "offset", "limit", "context")
+                .addStatement("return super.load$L($N, $N)", objectEntity.getName(), "offset", "context")
                 .build();
     }
 
@@ -211,7 +251,6 @@ public class EntityMigrationGenerator {
                 .addModifiers(Modifier.PROTECTED)
                 .returns(returnType)
                 .addParameter(Integer.class, "offset")
-                .addParameter(Integer.class, "limit")
                 .addParameter(ObjectContext.class, "context");
     }
 }
